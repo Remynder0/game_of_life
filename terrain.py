fb5d4f948fd1a513b91a045fb8bad4856784ffaf
Eliott Goubin <goubin221@unicaen.fr>
2023-03-24 14:21:15 +0100

    architecture des fichiers (Proto class)

--- terrain.py
+++ terrain.py
@@ -3,3 +3,21 @@ class Terrain:
     def __init__(self, height, weight):
         self.__height = height
         self.__weight = weight
+
+    def is_free_place(self, coordonnee):
+        pass
+    
+    def get_random_free_place(self):
+        pass
+    
+    def place_ressources(self, ressource):
+        pass
+
+    def place_animals(self, animals):
+        pass
+    
+    def peacefull_area(self):
+        pass
+    
+    def dangerous_area(self):
+        pass
