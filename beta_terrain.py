588a04891d33a277ed72d3c8d9538e91cb540dc4
Rémy Leber <leber222@unicaen.com>
2023-04-11 13:46:18 +0200

    maj beta_terrain

--- beta_terrain.py
+++ beta_terrain.py
@@ -27,11 +27,50 @@ class MonSprite(pygame.sprite.Sprite):
         if self.rect.top < 0 or self.rect.bottom > ecran.get_height():
             self.vy = -self.vy
 
+    def update_v(self):
+        self.rect.x += self.vx
+        self.rect.y += self.vy
+        if self.rect.left < 0 :
+            self.rect.x = ecran.get_width()
+        elif self.rect.right > ecran.get_width():
+            self.rect.x = 0
+        if self.rect.top < 0 :
+            self.rect.y = ecran.get_height()
+        elif self.rect.bottom > ecran.get_height():
+            self.rect.y = 0
+
+    def update_m(self):
+        self.rect.x += self.vx
+        self.rect.y += self.vy
+        if self.rect.left < 540 or self.rect.right > ecran.get_width():
+            self.vx = -self.vx
+        if self.rect.top < 0 or self.rect.bottom > ecran.get_height():
+            self.vy = -self.vy
+
+    def upate_o(self):
+        self.rect.x += self.vx
+        self.rect.y += self.vy
+        if self.rect.left < 0 or self.rect.right > 300:
+            self.vx = -self.vx
+        if self.rect.top < 0 or self.rect.bottom > 300:
+            self.vy = -self.vy
+
 def startGame():
     mon_sprite1 = MonSprite(100, 100)
     mon_sprite2 = MonSprite(200, 100)
     mon_sprite3 = MonSprite(200, 300)
+    mon_spriteV1 = MonSprite(800, 600)
+    mon_spriteV2 = MonSprite(800, 600)
+    mon_spriteM1 = MonSprite(600, 300)
+    mon_spriteM2 = MonSprite(600, 300)
+    mon_spriteO1 = MonSprite(200, 200)
+    mon_spriteO2 = MonSprite(200, 100)
+
     liste_sprites = [mon_sprite1, mon_sprite2, mon_sprite3]
+
+    liste_spritesV = [mon_spriteV1, mon_spriteV2]
+    liste_spritesM = [mon_spriteM1, mon_spriteM2]
+    liste_spritesO = [mon_spriteO1, mon_spriteO2]
 
     # Définir la taille et le nombre de régions
     TAILLE_REGION = 200
@@ -49,7 +88,7 @@ def startGame():
         x = sprite.rect.x // TAILLE_REGION
         y = sprite.rect.y // TAILLE_REGION
         regions[(x, y)].append(sprite)
-
+    i=0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -57,6 +96,7 @@ def startGame():
                 quit()
 
         # Tester les collisions pour les sprites dans les régions adjacentes
+        
         for sprite in liste_sprites:
             sprite.update()
             x = sprite.rect.x // TAILLE_REGION
@@ -66,12 +106,61 @@ def startGame():
                     if (x+dx, y+dy) in regions:
                         for other_sprite in regions[(x+dx, y+dy)]:
                             if sprite != other_sprite and sprite.rect.colliderect(other_sprite.rect):
-                                print("Collision !")
-                            else:
-                                print("RAS")
+                                sprite.vx = -sprite.vx
+                                print("Collision :",i)
+                                i+=1
+                        
+        for sprite in liste_spritesV:
+            sprite.update_v()
+            x = sprite.rect.x // TAILLE_REGION
+            y = sprite.rect.y // TAILLE_REGION
+            for dx in [-1, 0, 1]:
+                for dy in [-1, 0, 1]:
+                    if (x+dx, y+dy) in regions:
+                        for other_sprite in regions[(x+dx, y+dy)]:
+                            if sprite!= other_sprite and sprite.rect.colliderect(other_sprite.rect):
+                                print("Collision :",i)
+                                i+=1
+    
+        for sprite in liste_spritesM:
+            sprite.update_m()
+            x = sprite.rect.x // TAILLE_REGION
+            y = sprite.rect.y // TAILLE_REGION
+            for dx in [-1, 0, 1]:
+                for dy in [-1, 0, 1]:
+                    if (x+dx, y+dy) in regions:
+                        for other_sprite in regions[(x+dx, y+dy)]:
+                            if sprite!= other_sprite and sprite.rect.colliderect(other_sprite.rect):
+                                print("Collision :",i)
+                                i+=1
+
+        for sprite in liste_spritesO:
+            sprite.upate_o()
+            x = sprite.rect.x // TAILLE_REGION
+            y = sprite.rect.y // TAILLE_REGION
+            for dx in [-1, 0, 1]:
+                for dy in [-1, 0, 1]:
+                    if (x+dx, y+dy) in regions:
+                        for other_sprite in regions[(x+dx, y+dy)]:
+                            if sprite!= other_sprite and sprite.rect.colliderect(other_sprite.rect):
+                                print("Collision :",i)
+                                i+=1
+
+        
         ecran.fill((0, 0, 0))
+
+        #### affichage des sprites ####
         for sprite in liste_sprites:
             pygame.draw.rect(ecran, (255, 0, 0), sprite.rect)
+        for sprite in liste_spritesV:
+            pygame.draw.rect(ecran, (0, 255, 0), sprite.rect)
+        for sprite in liste_spritesM:
+            pygame.draw.rect(ecran, (0, 0, 255), sprite.rect)
+        for sprite in liste_spritesO:
+            pygame.draw.rect(ecran, (164, 82, 33), sprite.rect)
+        
+
+
         pygame.display.flip()
         clock.tick(60)
 
